04b4b16038a3ecb2369a0672a88298a9c2b120ac
MAHE0609 <maheshkumarreddy1206@gmail.com>
2025-01-24 14:17:07 +0530

    Add files via upload

---
+++ Omega ML1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("helloworld")
+
+
+# In[11]:
+
+
+import pandas as pd
+df=pd.read_csv(r"C:\Users\ramak\Downloads\customers-100.csv")
+df
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
